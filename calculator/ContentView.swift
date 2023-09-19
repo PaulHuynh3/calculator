51cc208c47da17655332e3dc2adfcaa6fd0f20b8
Paul <paul.huynh3@gmail.com>
2023-09-19 14:31:15 -0400

    final touches

--- calculator/ContentView.swift
+++ calculator/ContentView.swift
@@ -16,21 +16,36 @@ struct ContentView: View {
     ]
     
     var body: some View {
-        VStack {
-            ForEach(buttons, id: \.self) { row in
-                
+        ZStack(alignment: .bottom) {
+            Color.black.edgesIgnoringSafeArea(.all)
+            
+            VStack(spacing: 12) {
                 HStack {
-                    ForEach(row, id: \.self) { button in
-                        Text(button)
-                            .font(.system(size: 32))
-                            .frame(width: 80, height: 80)
+                    Spacer()
+                    Text("42")
+                        .foregroundColor(.white)
+                        .font(.system(size: 42))
+                }.padding()
+                ForEach(buttons, id: \.self) { row in
+                    HStack {
+                        ForEach(row, id: \.self) { button in
+                            Text(button)
+                                .font(.system(size: 32))
+                                .frame(width: self.buttonWidth(), height: self.buttonWidth())
+                                .foregroundColor(.white)
+                                .background(Color.yellow)
+                                .cornerRadius(self.buttonWidth())
+                        }
                     }
-                    
                 }
             }
         }
-        .padding()
     }
+    //5 gaps between 4 buttons
+    func buttonWidth() -> CGFloat {
+        return (UIScreen.main.bounds.width - 5 * 12) / 4
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
