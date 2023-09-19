efd04908018d3b3df729e10a918ad80d86aff918
Paul <paul.huynh3@gmail.com>
2023-09-19 12:53:29 -0400

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Paul Huynh on 2023-09-19.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
